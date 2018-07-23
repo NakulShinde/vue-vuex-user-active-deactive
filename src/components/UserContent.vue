14cc62b04e27eda9530cfcaa0fc8db4adab58599
NakulShinde <nakulshinde.it@gmail.com>
2018-07-23 23:21:44 +0530

    Getting userList from vuex store

--- src/components/UserContent.vue
+++ src/components/UserContent.vue
@@ -15,26 +15,14 @@
         components: {
             UsersList
         },
-        data : function (){
-            return {
-                userList: [
-                    { id: 0, isActive: true, name: 'Nakul Shinde' },
-                    { id: 1, isActive: true,  name: 'User 1' },
-                    { id: 2, isActive: true,  name: 'User 2' },
-                    { id: 3, isActive: true,  name: 'User 3' },
-                    { id: 4, isActive: true,  name: 'User 4' },
-                    { id: 5, isActive: true,  name: 'User 5' },
-                ],
-            }
-        },
         computed: {
             activeUserList: function () {
-                return this.userList.filter(function (user){
+                return this.$store.state.userList.filter(function (user){
                     return user.isActive == true;
                 })
             },
             deactiveUserList: function () {
-                return this.userList.filter(function (user){
+                return this.$store.state.userList.filter(function (user){
                     return user.isActive == false;
                 })
             } 
--- src/store.js
+++ src/store.js
@@ -5,7 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userList: [{
+        id: 0,
+        isActive: true,
+        name: 'Nakul Shinde'
+      },
+      {
+        id: 1,
+        isActive: true,
+        name: 'User 1'
+      },
+      {
+        id: 2,
+        isActive: true,
+        name: 'User 2'
+      },
+      {
+        id: 3,
+        isActive: true,
+        name: 'User 3'
+      },
+      {
+        id: 4,
+        isActive: true,
+        name: 'User 4'
+      },
+      {
+        id: 5,
+        isActive: true,
+        name: 'User 5'
+      },
+    ],
   },
   mutations: {
 
